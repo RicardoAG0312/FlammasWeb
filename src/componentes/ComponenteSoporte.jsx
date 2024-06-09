93d0e39a88e09a80b4209c75fc230e35fb52fd5d
UrquiaMartelJhoan <146016838+UrquiaMartelJhoan@users.noreply.github.com>
2024-06-08 19:10:31 -0500

    JHOAN
    AGREGUE LA PAGINA DE INICIO DE SERVICIOS PROFESIONALES Y SOPORTE TECNICO

--- src/componentes/ComponenteSoporte.jsx
+++ src/componentes/ComponenteSoporte.jsx
@@ -139,23 +139,65 @@ export function ComponenteSoporteProfesional () {
 export function ComponenteSoporteTecnico () {
     return (
         <>
-        <section className='container-fluid seccion-encabezado-soporte'>
-        <div className="row">
-            <div className="col-12 col-sm-8 col-xxl-8 primera-columna">
-                <div>
-                    <h1> SOPORTE TÉCNICO DE FLAMMAS </h1>
-                    <h2 > SOMOS EXPERTOS EN FLAMMAS </h2>
-                    <p> Garantizamos la continuidad del servicio y ofrecemos soporte a los equipos técnicos que gestionan Flammas. </p>
+            <section className='container-fluid seccion-encabezado-soporte'>
+                <div className="row">
+                    <div className="col-12 col-sm-7 col-xxl-7 primera-columna">
+                        <div>
+                            <h1> SOPORTE TÉCNICO DE FLAMMAS </h1>
+                            <h2> SOMOS EXPERTOS EN FLAMMAS </h2>
+                            <p> Garantizamos la continuidad del servicio y ofrecemos soporte a los equipos técnicos que gestionan Flammas. </p>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-5 col-xxl-5 seccion-contac-soporte ">
+                        <form action="/submit_form" method='POST'>
+                            <div>
+                                <label for="fname"> (*) Nombres: </label>
+                                <input type="text" id="fname" name="firstname" placeholder="Ingresa tus nombres" />
+                            </div>
+                            <div>
+                                <label for="lname"> (*) Apellidos: </label>
+                                <input type="text" id="lname" name="lastname" placeholder="Ingresa tus apellidos" />
+                            </div>
+                            <div>
+                                <label for="email"> (*) Email: </label>
+                                <input type="email" id="email" name="email" placeholder="Ingresa tu email" />
+                            </div>
+                            <div>
+                                <label for="company"> (*) Empresa - Compañia: </label>
+                                <input type="text" id="company" name="company" placeholder="Ingresa tu empresa/compañia" />
+                            </div>
+                            <div>
+                                <label for="country"> (*) País: </label>
+                                <input type="text" id="country" name="country" placeholder="Ingresa tu País" />
+                            </div>
+                            <div>
+                                <label for="message" className='mt-3'> (*) Mensaje: </label>
+                                <input type="text" id="message" name="message" placeholder="Ingresa tu mensaje" />
+                            </div>
+                            <div>
+                                <p> Flammas necesita la información de contacto que nos proporciona para ponernos en contacto contigo acerca de nuestros productos y servicios. Puedes darte de baja de estas comunicaciones en cualquier momento. Para obtener información sobre como darte de baja, asi como nuestras practicas de privacidad y el compromiso de proteger su privacidad, consulta nuestra Política de privacidad. </p>
+                                <div className = "recaptcha">
+                                    <ReCAPTCHA
+                                        ref={ReCAPTCHA}
+                                        sitekey="6Ldp0PMpAAAAAEWkyegdZWGYBaZxvOlpkeXOPEV6"
+                                        onChange={onChange}
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <input type="submit" value="Enviar" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </div>
-        </section>
+            </section>
         <ComponentetxtIzquierdaSoluciones
         texto="El objetivo principal del servicio de soporte técnico es brindar soporte para ayudar a resolver cualquier problema relacionado con la instalación, funcionamiento y uso de Pandora FMS."
         texto1="El canal de soporte principal disponible para todos los clientes de soporte es nuestro Sistema de soporte, que garantiza la mensajería en línea para una comunicación rápida y fácil entre los clientes y nuestros especialistas técnicos. Damos soporte en inglés y español:"
         texto2="Los clientes con niveles de soporte avanzados pueden acceder a los servicios de soporte por teléfono, llamando a números dedicados. El soporte 24/7 también está disponible para niveles de soporte avanzado."
         imagen="83"
         />
+        
     </>
     )
 }
